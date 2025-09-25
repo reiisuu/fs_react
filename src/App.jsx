597b44c2f3048d3338ab80c66c8fcf9c390727c1
Reisu Izumi <reisuizumi@gmail.com>
2025-09-25 14:28:43 +0800

    exercise 1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,30 @@
+import { useState } from 'react'
+
+const Header = (props) => <h1>{props.text}</h1>
+
+const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const Display = ({ text, number}) => <p>{text} {number}</p>
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-
-  ]
-}
-
-  const Header = ({course}) => {
-    return (
-    <>
-      <h1>{course.name}</h1>
-        <p>
-          {course.parts[0].name} {course.parts[0].exercises}
-        </p>
-    </>
-    )
-  }
-
-  const Content = ({course}) => {
-
-    const Part = (props) => {
-      return (
-      <>
-        <p>
-          { props.name } { props.exercises }
-        </p>
-      </>
-      )
-    }
-    return (
-      <>
-        <Part name = { course.parts[0].name } number = { course.parts[0].exercises }/>
-        <Part name = { course.parts[1].name } number = { course.parts[1].exercises }/>
-        <Part name = { course.parts[2].name } number = { course.parts[2].exercises }/>
-      </>
-    )
-  }
-
-  const Total = ({course}) => {
-    return (
-      <>
-        <p>
-        {course.parts[2].name} {course.parts[2].exercises}
-        </p>
-        <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-      </>
-    )
-  }
-  
   return (
     <div>
-      <Header course={course} />
-      <Content course={course}/>
-      <Total course={course} />
+      <Header text = 'give feedback' />
+
+      <Button onClick= {() => setGood(good + 1)} text = 'good'/>
+      <Button onClick= {() => setNeutral(neutral + 1)} text = 'neutral'/>
+      <Button onClick= {() => setBad(bad + 1)} text = 'bad'/>
+
+      <Header text = 'statistics' />
+
+      <Display text = 'good' number = {good} />
+      <Display text = 'neutral' number = {neutral} />
+      <Display text = 'bad' number = {bad} />
     </div>
   )
 }
