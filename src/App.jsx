b9010899dc67dedc109b77171cd5678fe4d5a366
Reisu Izumi <reisuizumi@gmail.com>
2025-09-26 13:50:00 +0800

    exercise 1.10

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 // const Display = ({ text, number }) => <p>{text} {number}</p>
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({good, neutral, bad, all, average, positive}) => {
   if (all === 0) 
     return (
@@ -14,12 +20,12 @@ const Statistics = ({good, neutral, bad, all, average, positive}) => {
 
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <StatisticLine text = 'good' value = {good} />
+      <StatisticLine text = 'neutral' value = {neutral} />
+      <StatisticLine text = 'bad' value = {bad} />
+      <StatisticLine text = 'all' value = {all} />
+      <StatisticLine text = 'average' value = {average} />
+      <StatisticLine text = 'positive' value = {positive} />
     </>
   )
 }
