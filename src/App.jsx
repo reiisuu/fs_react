2f82970b324954a4d462e302d5eccdad4aa03437
Reisu Izumi <reisuizumi@gmail.com>
2025-09-27 20:38:21 +0800

    exercise 1.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,53 @@
 import { useState } from 'react'
 
-const Header = (props) => <h1>{props.text}</h1>
+const Button = ({ onClick, selected}) => {
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-// const Display = ({ text, number }) => <p>{text} {number}</p>
-
-const StatisticLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({good, neutral, bad, all, average, positive}) => {
-  if (all === 0) 
-    return (
-      <p> No feedback given </p>
-  )
+console.log(selected);
 
   return (
     <>
-      <table>
-        <tbody>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={all} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={positive + ' %'} />
-        </tbody>
-      </table>
+      <button onClick = {onClick}>next anecdote</button>
+      
     </>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
-  const handleGood = () => {
-    const updatedGood = good + 1 // since react is async, the old is used if i don't assign it to a variable containing the updated value.
-    const updatedAll = all + 1
-
-    console.log('value of updated good: ', updatedGood)
-    console.log('value of updated neutral: ', neutral)
-    console.log('value of updated bad: ', bad)
-    console.log('value of updated all: ', updatedAll)
-
-    const newAverage = (updatedGood - bad) / updatedAll
-    const newPositive = updatedGood / updatedAll
-
-    setGood(updatedGood)
-    setAll(updatedAll)
-    setAverage(newAverage)
-    setPositive(newPositive)
-
-    console.log('new average: ', newAverage)
-    console.log('new positive: ', newPositive)
-  }
-
-  const handleNeutral = () => {
-    const updatedNeutral = neutral + 1
-    const updatedAll = all + 1
-
-    console.log('value of updated good: ', good)
-    console.log('value of updated neutral: ', updatedNeutral)
-    console.log('value of updated bad: ', bad)
-    console.log('value of updated all: ', updatedAll)
-
-    const newAverage = (good - bad) / updatedAll
-    const newPositive = good / updatedAll
-
-    setNeutral(updatedNeutral)
-    setAll(updatedAll)
-    setAverage(newAverage)
-    setPositive(newPositive)
-
-    console.log('new average: ', newAverage)
-    console.log('new positive: ', newPositive)
-  }
-
-  const handleBad = () => {
-    const updatedBad = bad + 1
-    const updatedAll = all + 1
-
-    console.log('value of updated good: ', good)
-    console.log('value of updated neutral: ', neutral)
-    console.log('value of updated bad: ', updatedBad)
-    console.log('value of updated all: ', updatedAll)
-
-    const newAverage = (good - updatedBad) / updatedAll
-    const newPositive = good / updatedAll
-
-    setBad(updatedBad)
-    setAll(updatedAll)
-    setAverage(newAverage)
-    setPositive(newPositive)
-
-    console.log('new average: ', newAverage)
-    console.log('new positive: ', newPositive)
+  const handleAnecdote = () => {
+    if (selected == anecdotes.length - 1)
+    {
+      setSelected(0);
+      console.log(selected);
+    }
+    else
+    {
+    
+      setSelected(selected + 1);
+      console.log(selected);
+    }
   }
 
   return (
-    <div>
-      <Header text = 'give feedback' />
+    <>
+      <div>
+        {anecdotes[selected]}
+      </div>
 
-      <Button onClick= {handleGood} text = 'good'/>
-      <Button onClick= {handleNeutral} text = 'neutral'/>
-      <Button onClick= {handleBad} text = 'bad'/>
-
-      <Header text = 'statistics' />
-
-      <Statistics good = {good} neutral = {neutral} bad = {bad} all = {all} average = {average} positive = {positive} />
-
-    </div>
+      <Button onClick = {handleAnecdote} selected = {selected} />
+    </>
   )
 }
 
